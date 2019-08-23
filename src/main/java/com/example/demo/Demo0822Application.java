b70ae23375a1d536278c93fef60b907be2a7f263
yuesezhenmeiya <2209680306@qq.com>
2019-08-23 12:11:54 +0800

    create project

---
+++ src/main/java/com/example/demo/Demo0822Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo0822Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo0822Application.class, args);
+    }
+
+}
